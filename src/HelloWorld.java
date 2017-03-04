8753041d419205eeadf1fe14f384146f14757e19
Tony Mao <s3602027@student.rmit.edu.au>
2017-03-05 00:24:19 +1100

    commit new class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@
 		public static void main(String[] args) {
 			System.out.println("Hello World!"); 
 			System.out.println("Test Push to Github"); 
+			System.out.println("Merge Forks Test."); 
 		}
 }
